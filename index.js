72b768adc67641c58cab0a0b8b0628502069b25b
nina <ninakartina.nina@gmail.com>
2018-01-15 14:30:11 +0700

    execute level 0 and 1

---
+++ index.js
@@ -0,0 +1,17 @@
+//LEVEL 0
+
+var numberA = 10
+let numberB = 099
+const numberC = 0.02
+
+console.log(numberA);
+console.log(numberB);
+console.log(numberC);
+
+//LEVEL 1
+console.log(numberA + numberB);
+console.log(numberA - numberB);
+console.log(numberA * numberB);
+console.log(numberA / numberB);
+
+//LEVEL 2
